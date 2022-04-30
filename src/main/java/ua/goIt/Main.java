73a77ed0eab59e56fbc8f144ee8ffd034488f1ec
ikuzmina1406 <ikuzmina1406@gmail.com>
2022-04-30 20:38:07 +0300

    Initial commit

---
+++ src/main/java/ua/goIt/Main.java
@@ -0,0 +1,13 @@
+package ua.goIt;
+
+class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Общая сумма покупок равна : " + FoodMarketService.calculateTotalCost("AaabccccDDBBB"));
+
+    }
+
+}
+
+
